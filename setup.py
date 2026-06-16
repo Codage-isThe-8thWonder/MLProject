7af2a2f0b50e717df825c1bcc05aea0d3cbf3c71
shreyash nagrale <shreyashnagrale260@gmail.com>
2026-06-16 19:11:04 +0530

    Built Exception and logger files

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_requirements(file_path:str)->List[str]:
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.replace("/n","") for req in requirements]
+        requirements = [req.replace("\n","") for req in requirements]
 
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
@@ -32,3 +32,21 @@ setup(
     install_requires= get_requirements("requirements.txt")
 
 )
+
+
+
+'''
+to build virtual virtual environment :
+    python -m venv .venv --without-pip
+    .venv\Scripts\Activate.ps1
+    python -m ensurepip --upgrade
+    pip --version
+
+
+Then you can install your al project dependencies by using :
+    pip install -r requirements.txt
+
+    
+pip install -e .   :-  "Current project ko install karo, lekin editable mode me."
+
+'''
